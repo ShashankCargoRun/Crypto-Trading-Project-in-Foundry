73894248fdbeff701228f77b2773969b84167936
ShashankCargoRun <shashank.cargo.run@gmail.com>
2026-09-15 17:35:00 +0000

    Initial commit

--- src/contracts/TokenPair.sol
+++ src/contracts/TokenPair.sol
@@ -18,3 +18,5 @@ contract TokenPair is ERC20, ReentrancyGuard, ITokenPair {
     uint256 private blockTimestampLast;
 
     uint256 public constant MINIMUM_LIQUIDITY = 10 ** 3;
+
+     bytes4 private constant SELECTOR = bytes4(keccak256(bytes("transfer(address,uint256)")));
